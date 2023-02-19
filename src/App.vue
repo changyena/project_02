f36035b2eef93a9661f3caba78001fc576768503
是长野呀 <11918737+its-nagano@user.noreply.gitee.com>
2023-02-20 01:52:59 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,115 @@
 <template>
   <div class="app-container">
-    <h1>App 根组件</h1>
+    <!-- Header头部区域 -->
+    <Header title="购物车案例"></Header>
+    
+    <!-- 循环渲染每一个商品的信息 -->
+    <Goods 
+    v-for="item in list" 
+    :key="item.id" 
+    :id="item.id"
+    :title="item.goods_name" 
+    :pic="item.goods_img" 
+    :price="item.goods_price"
+    :state="item.goods_state"
+    :count="item.goods_count"
+    @state-change="getNewState"
+    
+    
+    ></Goods>
+
+    <!-- footer区域 -->
+    <Footer :isfull="fullState" @full-change="getFullState" :amount="amt" :all="total"></Footer>
+   
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+import Header from '@/components/Header/Header.vue'
+import Goods from '@/components/Goods/Goods.vue'
+import Footer from '@/components/Footer/Footer.vue'
+import bus from '@/components/eventBus.js'
+
+
+
+
+export default {
+
+  data(){
+    return{
+      // 用来存储购物车的列表数据 默认为空数组
+      list:[]
+    }
+  },
+  components:{
+    Header,
+    Goods,
+    Footer
+   
+  },
+  methods: {
+    // 封装请求列表数据的方法
+   async initCartList(){
+    const{data:res} =await axios.get('https://www.escook.cn/api/cart')
+    if(res.status ===200){
+      this.list = res.list
+    }
+    },
+  getNewState(e){
+    // console.log(val);
+    this.list.some(item=>{
+      if(item.id===e.id){
+        item.goods_state=e.value
+        return true
+      }
+    })
+  },
+  getFullState(val){
+    // console.log(val);
+    this.list.forEach(item=>(item.goods_state=val))
+
+  }
+
+
+  },
+  computed:{
+    // 动态计算出全选的状态是true还是false
+    fullState(){
+     return this.list.every(item=>item.goods_state)
+
+    },
+    // 已勾选商品的总价格
+    amt(){
+     return this.list.filter(item=>item.goods_state).reduce((total,item)=>{
+      return  total+=item.goods_price*item.goods_count
+      },0)
+
+    },
+    // 已勾选商品的总数量
+    total(){
+     return this.list.filter(item=>item.goods_state).reduce((t,item)=>{
+       return t+=item.goods_count
+      },0)
+
+    }
+
+  },
+  created(){
+    this.initCartList()
+    bus.$on('share',(val)=>{
+      // console.log('接收到了值',val);
+      this.list.some(item=>{
+        if(item.id===val.id){
+          item.goods_count=val.value
+          return true
+        }
+      })
+    })
+   
+  },
+  
+}
 </script>
 
 <style lang="less" scoped>
--- src/components/Counter/Counter.vue
+++ src/components/Counter/Counter.vue
@@ -1,16 +1,46 @@
 <template>
   <div class="number-container d-flex justify-content-center align-items-center">
     <!-- 减 1 的按钮 -->
-    <button type="button" class="btn btn-light btn-sm">-</button>
+    <button type="button" class="btn btn-light btn-sm" @click="sub">-</button>
     <!-- 购买的数量 -->
-    <span class="number-box">1</span>
+    <span class="number-box">{{ num }}</span>
     <!-- 加 1 的按钮 -->
-    <button type="button" class="btn btn-light btn-sm">+</button>
+    <button type="button" class="btn btn-light btn-sm" @click="add">+</button>
   </div>
 </template>
 
 <script>
-export default {}
+import bus from '@/components/eventBus.js'
+export default {
+  props:{
+    // 接收商品的id值 将来使用EventBus方案 把数量传递到App.vue的时候 需要通知App组件 需要更新那个商品的数量
+    id:{
+      type:Number,
+      required:true
+    },
+    // 接收到的num数量值
+    num:{
+      type:Number,
+      default:1
+    }
+  },
+  methods: {
+    // 点击按钮 让数值加1
+    add(){
+      const obj = {id:this.id,value:this.num+1}
+      // console.log(obj);
+      bus.$emit('share',obj)
+    },
+    sub(){
+      if(this.num-1===0) return
+      const obj = {id:this.id,value:this.num-1}
+      // console.log(obj);
+      bus.$emit('share',obj)
+
+    }
+   
+  },
+}
 </script>
 
 <style lang="less" scoped>
